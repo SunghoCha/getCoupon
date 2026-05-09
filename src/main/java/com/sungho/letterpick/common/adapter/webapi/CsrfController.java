21ace5cfc7ade8ea869eabf736bebaf307e0c891
SunghoCha <tjdgh3725@gmail.com>
2026-05-09 23:04:37 +0900

    docs: CSRF 토큰 발급 의도 설명 추가

--- src/main/java/com/sungho/letterpick/common/adapter/webapi/CsrfController.java
+++ src/main/java/com/sungho/letterpick/common/adapter/webapi/CsrfController.java
@@ -11,6 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/csrf")
 public class CsrfController {
 
+    /**
+     * 응답 본문 없이 CSRF 쿠키만 준비한다.
+     * {@link CsrfToken#getToken()} 호출은 csrf 메서드에서 토큰 생성을 확정하고
+     * XSRF-TOKEN 쿠키가 내려가게 하기 위한 용도다.
+     */
     @GetMapping
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void csrf(CsrfToken csrfToken) {
